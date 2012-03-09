33278b2a616917a826d70a2861d22aa6fe5b6a28
Ettore <speziale.ettore@gmail.com>
2012-03-09 17:13:27 +0100

    Implemented the HelloLLVM pass.

--- lib/HelloLLVM/HelloLLVM.cpp
+++ lib/HelloLLVM/HelloLLVM.cpp
@@ -1,7 +1,11 @@
 
 #include "cot/AllPasses.h"
 
+#include "llvm/Constants.h"
+#include "llvm/Instructions.h"
+#include "llvm/Module.h"
 #include "llvm/Support/ErrorHandling.h"
+#include "llvm/Type.h"
 
 using namespace cot;
 
@@ -26,6 +30,12 @@ public:
   virtual const char *getPassName() const {
     return "Hello llvm";
   }
+
+private:
+  llvm::Function &GetHelloWorld(llvm::Module &Mod, bool &Built);
+  void AddMain(llvm::Module &Mod, llvm::Function &HelloFun, bool &Modified);
+
+  llvm::Function &GetPrintf(llvm::Module &Mod);
 };
 
 char HelloLLVM::ID = 0;
@@ -33,11 +43,131 @@ char HelloLLVM::ID = 0;
 } // End anonymous namespace.
 
 bool HelloLLVM::runOnModule(llvm::Module &Mod) {
-  llvm_unreachable("Not yet implemented");
+  bool HelloWorldBuilt, MainBuilt;
+
+  llvm::Function &HelloWorld = GetHelloWorld(Mod, HelloWorldBuilt);
+  AddMain(Mod, HelloWorld, MainBuilt);
+
+  // This method must return true if the visited module was modified.
+  return HelloWorldBuilt || MainBuilt;
 }
 
 void HelloLLVM::getAnalysisUsage(llvm::AnalysisUsage &AU) const {
-  llvm_unreachable("Not yet implemented");
+  // Normally here we have to require analysis -- AU.addRequired -- and declare
+  // preserved analysis -- AU.setPreserved. However, this pass does no require
+  // any analysis and potentially invalidates all analysis. The default
+  // behaviour is to invalidate all analysis.
+}
+
+llvm::Function &HelloLLVM::GetHelloWorld(llvm::Module &Mod, bool &Modified) {
+  llvm::Function *Fun = Mod.getFunction("hello_world");
+
+  // Function already available.
+  if(Fun && !Fun->isDeclaration()) {
+    Modified = false;
+    return *Fun;
+  }
+
+  // A context is used to track the compiler status -- it is required passing it
+  // to function creating new LLVM objects in order to be inserted into compiler
+  // state.
+  llvm::LLVMContext &Ctx = Mod.getContext();
+
+  llvm::Constant *FunSym;
+
+  // No code associated to the function, first insert a declaration.
+  FunSym = Mod.getOrInsertFunction("hello_world",
+                                   llvm::Type::getVoidTy(Ctx),
+                                   NULL);
+  Fun = llvm::cast<llvm::Function>(FunSym);
+
+  // Then build a basic block to hold instructions.
+  llvm::BasicBlock *Entry = llvm::BasicBlock::Create(Ctx, "entry", Fun);
+
+  llvm::GlobalVariable *Msg;
+  llvm::Value *MsgPtr;
+
+  // We want to generate 'printf("Hello, World!")'. First parameter is a
+  // constant.
+  llvm::Constant *MsgInit = llvm::ConstantArray::get(Ctx, "Hello, World!\n");
+
+  // Put it into a global variable.
+  Msg = new llvm::GlobalVariable(Mod,
+                                 MsgInit->getType(),
+                                 true,
+                                 llvm::GlobalVariable::InternalLinkage,
+                                 MsgInit,
+                                 "hello_world.msg");
+
+  // Actually, 'printf' printf first parameter is a pointer -- we need to get
+  // its address. Get an offsets vector first.
+  llvm::SmallVector<llvm::Value *, 2> Offsets;
+  Offsets.assign(2, llvm::ConstantInt::get(llvm::Type::getInt32Ty(Ctx), 0));
+
+  // Then, use it to perform some pointer arithmetics.
+  MsgPtr = llvm::GetElementPtrInst::Create(Msg, Offsets, "", Entry);
+
+  // Now we need the 'printf' function.
+  llvm::Function &Printf = GetPrintf(Mod);
+
+  // Call it, passing the string as the only argument.
+  llvm::CallInst::Create(&Printf, MsgPtr, "", Entry);
+
+  // A Basic block must always ends with a terminator instruction. In this case
+  // a return.
+  llvm::ReturnInst::Create(Ctx, Entry);
+
+  Modified = true;
+  return *Fun;
+}
+
+void HelloLLVM::AddMain(llvm::Module &Mod,
+                        llvm::Function &HelloFun,
+                        bool &Modified) {
+  llvm::Function *Fun = Mod.getFunction("main");
+
+  // Function already available.
+  if(Fun && !Fun->isDeclaration()) {
+    Modified = false;
+    return;
+  }
+
+  llvm::LLVMContext &Ctx = Mod.getContext();
+  llvm::Type *VoidTy = llvm::Type::getVoidTy(Ctx);
+
+  llvm::Constant *FunSym;
+
+  // Main not inside the compilation unit -- add it.
+  FunSym = Mod.getOrInsertFunction("main", VoidTy, NULL);
+  Fun = llvm::cast<llvm::Function>(FunSym);
+
+  // Make an entry block.
+  llvm::BasicBlock *Entry = llvm::BasicBlock::Create(Ctx, "entry", Fun);
+
+  // Call the greeter and return.
+  llvm::CallInst::Create(&HelloFun, llvm::ArrayRef<llvm::Value *>(), "", Entry);
+  llvm::ReturnInst::Create(Ctx, Entry);
+
+  Modified = true;
+}
+
+// The signature of 'printf' is architecture specific -- use this member
+// function to define signatures with respect to the architecture.
+llvm::Function &HelloLLVM::GetPrintf(llvm::Module &Mod) {
+  llvm::LLVMContext &Ctx = Mod.getContext();
+  llvm::FunctionType *PrintfTy;
+
+#if defined(__i386__) || defined(__X86_64__)
+  PrintfTy = llvm::FunctionType::get(llvm::Type::getInt32Ty(Ctx),
+                                     llvm::Type::getInt8PtrTy(Ctx),
+                                     true);
+#else
+#error "Architecture not supported"
+#endif
+
+  llvm::Constant *FunSym = Mod.getOrInsertFunction("printf", PrintfTy);
+
+  return *llvm::cast<llvm::Function>(FunSym);
 }
 
 llvm::Pass *cot::CreateHelloLLVMPass() { return new HelloLLVM(); }
--- test/COTPasses/HelloLLVM/AddHello.ll
+++ test/COTPasses/HelloLLVM/AddHello.ll
@@ -10,3 +10,20 @@ target triple = "i386-pc-linux-gnu"
 define void @foo() nounwind {
   ret void
 }
+
+; CHECK: @hello_world.msg = internal constant [15 x i8] c"Hello, World!\0A\00"
+
+; CHECK:      define void @hello_world() {
+; CHECK-NEXT: entry:
+; CHECK-NEXT:   %0 = getelementptr [15 x i8]* @hello_world.msg, i32 0, i32 0
+; CHECK-NEXT:   %1 = call i32 (i8*, ...)* @printf(i8* %0)
+; CHECK-NEXT:   ret void
+; CHECK-NEXT: }
+
+; CHECK: declare i32 @printf(i8*, ...)
+
+; CHECK:      define void @main() {
+; CHECK-NEXT: entry:
+; CHECK-NEXT:   call void @hello_world()
+; CHECK-NEXT:   ret void
+; CHECK-NEXT: }
